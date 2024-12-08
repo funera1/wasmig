6e83d876779e233501f43d9d5ec3f831fe1c9850
funera1 <funeral7776@gmail.com>
2024-12-08 09:49:31 +0900

    Add checkpoint program counter to header file

--- include/wasmig/migration.h
+++ include/wasmig/migration.h
@@ -10,5 +10,6 @@ void hello_world();
 
 int checkpoint_memory(uint8_t* memory, uint32_t cur_page);
 int checkpoint_global(uint64_t* values, uint32_t* types, int len);
+int checkpoint_pc(uint32_t func_idx, uint32_t offset);
 
 #endif
