5f5468a39b363461e1d6810c436ae08e6f7a0f8b
funera1 <funeral7776@gmail.com>
2024-12-23 08:05:19 +0900

    Fix

--- include/wasmig/migration.h
+++ include/wasmig/migration.h
@@ -37,7 +37,7 @@ typedef struct labels {
 
 int checkpoint_memory(uint8_t* memory, uint32_t cur_page);
 int checkpoint_global(uint64_t* values, uint32_t* types, int len);
-bool tab_get(uintptr_t address, CodePos *out);
+int checkpoint_pc(uint32_t func_idx, uint32_t offset);
 int checkpoint_stack(uint32_t call_stack_id, uint32_t entry_fidx, 
     CodePos *ret_addr, CodePos *cur_addr, Array32 *locals, Array32 *value_stack, LabelStack *label_stack, bool is_top);
 int checkpoint_call_stack_size(uint32_t call_stack_size);
--- include/wasmig/table.h
+++ include/wasmig/table.h
@@ -14,7 +14,7 @@ extern "C" {
 #endif
 
 int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address);
-CodePos tab_get(uintptr_t address);
+bool tab_get(uintptr_t address, CodePos *out);
 int set_cur_fidx(uint32_t fidx);
 uint32_t get_cur_fidx();
 
