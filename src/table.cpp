b5c2703ae955a4257317e7b387f9392219ea5fc3
funera1 <funeral7776@gmail.com>
2024-12-22 22:45:23 +0900

    Delete comment out

--- src/table.cpp
+++ src/table.cpp
@@ -1,27 +1,5 @@
 #include "table.h"
 
-// static inline int is_allocated(uint32_t func_idx) {
-//     return (func_idx < gtable.size());
-// }
-
-// int tab_alloc(uint32_t func_idx) {
-//     // gtable[func_idx]がすでに確保されている
-//     if (is_allocated(func_idx)) {
-//         return 0;
-//     }
-
-//     // 不足分を確保する
-//     std::vector<table> newmaps(func_idx-gtable.size());
-//     gtable.insert(gtable.end(), newmaps.begin(), newmaps.end());
-//     return 0;
-// }
-
-// int tab_detroy() {
-//     // gtableのメモリを開放する
-//     std::vector<table>().swap(gtable);
-//     return 0;
-// }
-
 int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address) {
     gtable[address] = CodePos(func_idx, offset);
     return 0;
