371069479a460371a1bb6c6d8d9d65d294ba0513
funera1 <funeral7776@gmail.com>
2024-12-22 23:19:53 +0900

    Fix compile error

--- include/wasmig/table.h
+++ include/wasmig/table.h
@@ -7,8 +7,19 @@ static uint32_t cur_fidx;
 
 // extern "C" int tab_alloc(uint32_t func_idx);
 // extern "C" int tab_destroy();
-extern "C" int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address);
-extern "C" CodePos tab_get(uintptr_t address);
+// extern "C" int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address);
+// extern "C" CodePos tab_get(uintptr_t address);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address);
+CodePos tab_get(uintptr_t address);
+
+#ifdef __cplusplus
+}
+#endif
 
 int set_cur_fidx(uint32_t fidx);
 uint32_t get_cur_fidx();
