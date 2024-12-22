7563cfe63eaf9d347d511d2aa19e62b7552eda6f
funera1 <funeral7776@gmail.com>
2024-12-22 22:59:29 +0900

    Add cur_fidx setter getter

--- include/wasmig/table.h
+++ include/wasmig/table.h
@@ -3,9 +3,14 @@
 
 #include "migration.h"
 
+static uint32_t cur_fidx;
+
 // extern "C" int tab_alloc(uint32_t func_idx);
 // extern "C" int tab_destroy();
 int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address);
 CodePos tab_get(uintptr_t address);
+
+int set_cur_fidx(uint32_t fidx) {cur_fidx = fidx;}
+uint32_t get_cur_fidx() {return cur_fidx;}
 
 #endif
