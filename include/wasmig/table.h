1d553df105eb94ba0227b9566869ae0adeddc25b
funera1 <funeral7776@gmail.com>
2024-12-22 22:32:06 +0900

    Implement address table

--- include/wasmig/table.h
+++ include/wasmig/table.h
@@ -5,12 +5,13 @@
 #include <vector>
 #include <map>
 
-using table = std::map<uintptr_t, CodePos>;
-static std::vector<table> gtable;
+// using table = std::map<uintptr_t, CodePos>;
+// static std::vector<table> gtable;
+static std::map<uintptr_t, CodePos> gtable;
 
-extern "C" int t_alloc(uint32_t func_idx);
-extern "C" int t_destroy();
-extern "C" int t_register(uint32_t func_idx, uint64_t offset, uintptr_t addoress);
-extern "C" CodePos t_get(uintptr_t address);
+extern "C" int tab_alloc(uint32_t func_idx);
+extern "C" int tab_destroy();
+extern "C" int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address);
+extern "C" CodePos tab_get(uintptr_t address);
 
 #endif
--- src/table.cpp
+++ src/table.cpp
@@ -1,19 +1,37 @@
 #include "table.h"
 
-int t_alloc(uint32_t func_idx) {
-    // gtable[func_idx]がすでに確保されている
-    if (func_idx < gtable.size()) {
-        return 0;
-    }
+// static inline int is_allocated(uint32_t func_idx) {
+//     return (func_idx < gtable.size());
+// }
 
-    // 不足分を確保する
-    std::vector<table> newmaps(func_idx-gtable.size());
-    gtable.insert(gtable.end(), newmaps.begin(), newmaps.end());
+// int tab_alloc(uint32_t func_idx) {
+//     // gtable[func_idx]がすでに確保されている
+//     if (is_allocated(func_idx)) {
+//         return 0;
+//     }
+
+//     // 不足分を確保する
+//     std::vector<table> newmaps(func_idx-gtable.size());
+//     gtable.insert(gtable.end(), newmaps.begin(), newmaps.end());
+//     return 0;
+// }
+
+// int tab_detroy() {
+//     // gtableのメモリを開放する
+//     std::vector<table>().swap(gtable);
+//     return 0;
+// }
+
+int tab_set(uint32_t func_idx, uint64_t offset, uintptr_t address) {
+    // gtable[func_idx]が確保されているかcheck
+    // if (!is_allocated(func_idx)) {
+    //     return 1;
+    // }
+
+    gtable[address] = CodePos(func_idx, offset);
     return 0;
 }
 
-int t_detroy() {
-    // gtableのメモリを開放する
-    std::vector<table>().swap(gtable);
-    return 0;
+CodePos tab_get(uintptr_t address) {
+    return gtable[address];
 }
