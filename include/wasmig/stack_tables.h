367f2c068328348ee1fc93c4bc30f1c5678c0832
funera1 <funeral7776@gmail.com>
2025-04-13 17:14:08 +0900

    Fix

--- include/wasmig/stack_tables.h
+++ include/wasmig/stack_tables.h
@@ -1,3 +1,5 @@
 #pragma once
+#include <cstddef>
+#include <cstdint>
 
 size_t get_stack_size(uint32_t fidx, uint64_t offset);
--- src/stack_tables.cpp
+++ src/stack_tables.cpp
@@ -1,4 +1,4 @@
-#include "stack_tables.h"
+#include "wasmig/stack_tables.h"
 #include <wcrn.h>
 
 size_t get_stack_size(uint32_t fidx, uint64_t offset) {
