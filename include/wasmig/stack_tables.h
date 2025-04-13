927c0359184571f63513ad435db5e8231630d904
funera1 <funeral7776@gmail.com>
2025-04-13 17:12:04 +0900

    Fix dependencies

--- include/wasmig/stack_tables.h
+++ include/wasmig/stack_tables.h
@@ -1,6 +1,3 @@
 #pragma once
-#include <wcrn.h>
 
-size_t get_stack_size(uint32_t fidx, uint64_t offset) {
-    return wcrn_get_stack_size(fidx, offset);
-}
+size_t get_stack_size(uint32_t fidx, uint64_t offset);
---
+++ src/stack_tables.cpp
@@ -0,0 +1,6 @@
+#include "stack_tables.h"
+#include <wcrn.h>
+
+size_t get_stack_size(uint32_t fidx, uint64_t offset) {
+    return wcrn_get_stack_size(fidx, offset);
+}
