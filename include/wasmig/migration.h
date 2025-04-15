e04a1bc5555fd552a1c19c2c4fc906d4c53d32c7
funera1 <funeral7776@gmail.com>
2025-04-15 16:42:37 +0900

    Add spdlog debug mode

--- include/wasmig/migration.h
+++ include/wasmig/migration.h
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <spdlog/spdlog.h>
 
 void hello_world();
 
@@ -45,6 +46,10 @@ typedef struct callstack_entry {
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+void spdlog_debug() {
+    spdlog::set_level(spdlog::level::debug);
+}
 
 int checkpoint_memory(uint8_t* memory, uint32_t cur_page);
 int checkpoint_global(uint64_t* values, uint32_t* types, int len);
