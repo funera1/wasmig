48c269ccb31568e70fef9d2bd6f270b2fe62c7ee
funera1 <funeral7776@gmail.com>
2025-04-15 16:43:53 +0900

    Fix spdlog implementation

--- include/wasmig/migration.h
+++ include/wasmig/migration.h
@@ -9,7 +9,6 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <spdlog/spdlog.h>
 
 void hello_world();
 
@@ -47,9 +46,7 @@ typedef struct callstack_entry {
 extern "C" {
 #endif
 
-void spdlog_debug() {
-    spdlog::set_level(spdlog::level::debug);
-}
+void spdlog_debug();
 
 int checkpoint_memory(uint8_t* memory, uint32_t cur_page);
 int checkpoint_global(uint64_t* values, uint32_t* types, int len);
