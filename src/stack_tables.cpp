35380fe0e0e161ed588e772438e0a9c9bc046dfa
funera1 <funeral7776@gmail.com>
2025-04-16 17:46:11 +0900

    Add get_stack_table interface

--- src/stack_tables.cpp
+++ src/stack_tables.cpp
@@ -3,7 +3,14 @@
 
 
 extern "C" {
-
+    
+StackTable get_stack_table(uint32_t fidx, uint64_t offset) {
+    StackTable stack_table;
+    // stack_table.locals = wcrn_get_locals(fidx, offset);
+    // stack_table.value_stack = wcrn_get_value_stack(fidx, offset);
+    // stack_table.label_stack = wcrn_get_label_stack(fidx, offset);
+    return stack_table;
+}
 size_t get_stack_size(uint32_t fidx, uint64_t offset) {
     return wcrn_get_stack_size(fidx, offset);
 }
