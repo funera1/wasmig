d7d6c5f4bf6fcc71aa397c6d9a648b251afda00c
funera1 <funeral7776@gmail.com>
2025-04-18 22:51:20 +0900

    Restore stack

--- src/restore.cpp
+++ src/restore.cpp
@@ -1,6 +1,6 @@
-// #include <wasmig/utils.h>
-// #include <wasmig/migration.h>
-// #include <spdlog/spdlog.h>
+#include <wasmig/utils.h>
+#include <wasmig/migration.h>
+#include <spdlog/spdlog.h>
 
 // void restore_dirty_memory(uint8_t *memory, FILE* memory_fp) {
 //     const int PAGE_SIZE = 4096;
@@ -63,66 +63,24 @@
 //     return globals;
 // }
 
-// CallStack restore_stack() {
-//     FILE *fp = open_image("frame.img", "rb");
-//     if (fp == NULL) {
-//         spdlog::error("failed to open frame file");
-//         return {0, NULL};
-//     }
-//     uint32_t call_stack_size;
-//     fread(&call_stack_size, sizeof(uint32_t), 1, fp);
-//     fclose(fp);
-    
-//     CallStack call_stack;
-//     call_stack.size = call_stack_size;
-//     call_stack.entries = (CallStackEntry*)malloc(sizeof(CallStackEntry) * call_stack_size);
-    
-//     for (int i = 0; i < call_stack_size; ++i) {
-//         char file[32];
-//         // TODO: stack_%d.imgに変更する
-//         snprintf(file, sizeof(file), "stack%d.img", i+1);
-//         FILE *fp = open_image(file, "rb");
-//         if (fp == NULL) {
-//             spdlog::error("failed to open frame file {}", file);
-//             return {0, NULL};
-//         }
-
-//         // entry_fidx
-//         uint32_t entry_fidx;
-//         fread(&entry_fidx, sizeof(uint32_t), 1, fp);
-
-//         // return address
-//         CodePos ret_pos;
-//         fread(&ret_pos.fidx, sizeof(uint32_t), 1, fp);
-//         fread(&ret_pos.offset, sizeof(uint32_t), 1, fp);
-        
-//         // 型スタック
-//         Array8 type_stack;
-//         fread(&type_stack.size, sizeof(uint32_t), 1, fp);
-//         type_stack.contents = (uint8_t*)malloc(sizeof(uint8_t) * type_stack.size);
-//         fread(type_stack.contents, sizeof(uint8_t), type_stack.size, fp);
-
-//         // ローカル&値スタック
-//         Array32 locals, value_stack;
-//         fread(&locals.size, sizeof(uint32_t), 1, fp);
-//         locals.contents = (uint32_t*)malloc(sizeof(uint32_t) * locals.size);
-//         fread(locals->contents, sizeof(uint32_t), locals->size, fp);
-
-//         fread(&value_stack->size, sizeof(uint32_t), 1, fp);
-//         value_stack->contents = (uint32_t*)malloc(sizeof(uint32_t) * value_stack->size);
-//         fread(value_stack->contents, sizeof(uint32_t), value_stack->size, fp);
-
-//         fread(&label_stack->size, sizeof(uint32_t), 1, fp);
-//         label_stack->begins = (uint32_t*)malloc(sizeof(uint32_t) * label_stack->size);
-//         label_stack->targets = (uint32_t*)malloc(sizeof(uint32_t) * label_stack->size);
-//         label_stack->stack_pointers = (uint32_t*)malloc(sizeof(uint32_t) * label_stack->size);
-//         label_stack->cell_nums = (uint32_t*)malloc(sizeof(uint32_t) * label_stack->size);
-//         for (int i = 0; i < label_stack->size; ++i) {
-//             fread(label_stack->begins, sizeof(uint32_t), 1, fp);
-//             fread(label_stack->targets, sizeof(uint32_t), 1, fp);
-//             fread(label_stack->stack_pointers, sizeof(uint32_t), 1, fp);
-//             fread(label_stack->cell_nums, sizeof(uint32_t), 1, fp);
-//         }
-//     }
-//     return call_stack; 
-// }
+CallStack restore_stack() {
+    FILE *fp = open_image("call_stack.img", "rb");
+    if (fp == NULL) {
+        spdlog::error("failed to open call stack file");
+        return {0, NULL};
+    }
+    // fpからデータを読み込む
+    fseek(fp, 0, SEEK_END);
+    size_t file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    uint8_t *buf = (uint8_t *)malloc(file_size);
+    fread(buf, file_size, 1, fp);
+    fclose(fp);
+    // bufをデシリアライズする
+    Array8 array8;
+    array8.size = file_size;
+    array8.contents = buf;
+    CallStack call_stack = deserialize_call_stack(&array8);
+    free(buf);
+    return call_stack;
+}
