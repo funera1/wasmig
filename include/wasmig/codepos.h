51e7b034adeb6e4a83dff36d0235cfd691d5b7d6
funera1 <funeral7776@gmail.com>
2025-04-19 20:50:03 +0900

    Fix syntax error

--- include/wasmig/codepos.h
+++ include/wasmig/codepos.h
@@ -1,4 +1,12 @@
 #include "wasmig/state.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 CodePos prev_pc(CodePos pc);
 CodePos next_pc(CodePos pc);
+
+#ifdef __cplusplus
+}
+#endif
--- src/codepos.cpp
+++ src/codepos.cpp
@@ -1,9 +1,10 @@
 #include "wasmig/state.h"
 #include "wasmig/stack_tables.h"
 #include "wcrn.h"
+#include <spdlog/spdlog.h>
 
 extern "C" {
-    CodePos prev(CodePos pc) {
+    CodePos prev_pc(CodePos pc) {
         Array32 offs = wcrn_offset_list(pc.fidx);
         for (int i = 0; i < offs.size; i++) {
             if (pc.offset == offs[i]) {
@@ -18,15 +19,15 @@ extern "C" {
         spdlog::warn("This pc is inccorrect");
         return pc;
     }
-    CodePos next(CodePos pc) {
+    CodePos next_pc(CodePos pc) {
         Array32 offs = wcrn_offset_list(pc.fidx);
         for (int i = 0; i < offs.size; i++) {
             if (pc.offset == offs[i]) {
                 if (i > 0) offset = offs[i+1];
                 else offset = offs[i];
                 return CodePos {
-                    .fidx: fidx
-                    .offset: offset
+                    .fidx = fidx,
+                    .offset = offset,
                 };
             }
         }
